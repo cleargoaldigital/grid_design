fde2d0ff79a272ceaf28aba40a5a592766dcea09
cleargoaldigital <cleargoal.ng@gmail.com>
2022-05-01 08:48:53 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const heading = document.querySelectorAll('h1');
+for(let i = 0; i <heading.length; i++){
+   heading[i].innerText = "Simple Grid Designs";
+}
+
+// const targethead = document.querySelectorAll('section[target]');
+// for(let i = 0; i < targethead.length; i++){
+//    targethead[i].style.border = "10px solid red";
+// }
